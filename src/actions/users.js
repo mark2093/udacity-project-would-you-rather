1f4eb187e74065abc515533f569b0742d6121c09
mark2093 <markdsouza93@gmail.com>
2020-04-12 09:16:49 +0530

    feat: Actions/users

---
+++ src/actions/users.js
@@ -0,0 +1,61 @@
+import { _getUsers } from '../utils/_DATA'
+import {showLoading, hideLoading} from 'react-redux-loading';
+
+export const SAVE_A_QUESTION = 'SAVE_USER_QUESTION'
+export const REMOVE_A_QUESTION = 'REMOVE_USER_QUESTION'
+export const SAVE_A_ANSWER = 'SAVE_USER_ANSWER'
+export const REMOVE_A_ANSWER = 'REMOVE_USER_ANSWER'
+export const USERS_UPDATE = 'USERS_UPDATE'
+
+export function saveAQuestion (authedUser, ques_id) {
+    return {
+      type: SAVE_A_ANSWER,
+      authedUser,
+      ques_id,
+    }
+  }
+  
+  export function removeAQuestion (authedUser, ques_id) {
+    return {
+      type: SAVE_A_QUESTION,
+      authedUser,
+      ques_id,
+    }
+  }
+  
+  export function saveAAnswer (authedUser, ques_id, answer_opt) {
+    return {
+      type: SAVE_A_ANSWER,
+      authedUser,
+      ques_id,
+      answer_opt
+    }
+  }
+  
+  export function removeAAnswer (authedUser, ques_id, answer_opt) {
+    return {
+      type: REMOVE_A_ANSWER,
+      authedUser,
+      ques_id,
+      answer_opt
+    }
+  }
+  
+  
+  export function usersUpdate (authedUsers) {
+    return {
+      type: USERS_UPDATE,
+      authedUsers,
+    }
+  }
+  
+    export function getUsers () {
+    return (dispatch) => {
+        dispatch(showLoading());
+      return _getUsers()
+        .then((users) => {
+        dispatch(usersUpdate(users));
+        dispatch(hideLoading());
+      })
+    }
+  }
